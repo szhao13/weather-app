c49358b1c483a102da08eb5d05641f0d63ced1e3
Shelley Zhao <shelleyzhao@Shelleys-MacBook-Pro.local>
2018-11-20 23:54:57 -0500

    some syntax

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const apiKey = '0a5d5fd45c0ef7b565b787396c87e2f4';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
   res.render('index', {weather: null, error: null});
